3e8f4437c3b574300eafe7515db354294a54df22
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-02-12 19:17:41 +0900

    add bitflyerapi

--- api_base.py
+++ api_base.py
@@ -1,5 +1,6 @@
 from coinall.CoinallAPI import CoinallAPI
 from bitbank.BitBankAPI import BitBankPrvAPI
+from bitflyer.BitflyerAPI import BitflyerAPI
 
 from utils.Config import Config
 
@@ -33,3 +34,9 @@ bitbankapi.get_trade("btc_jpy")
 bitbankapi.get_trade("mona_jpy")
 
 bitbankapi.get_withdraw("btc")
+
+API_KEY = config['BANKINFO']['BITFLYER']['API_KEY']
+API_SECRET = config['BANKINFO']['BITFLYER']['SECRET_KEY']
+
+bitflyerapi = BitflyerAPI(API_KEY, API_SECRET)
+
